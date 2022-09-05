395e2ab3c2638aee203b15886f5b2bfaaa2124a0
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-09-05 10:26:10 +0300

    Initial commit

---
+++ src/TekBoyutluDiziler.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // Array
+        // VeriTipi diziAdı[] = new VeriTipi[elemanSayisi];
+        // VeriTipi[] diziAdi = new VeriTipi[elemanSayisi];
+        // VeriTipi diziAdi ={v1,v2,v3};
+
+        int[] liste = new int[4];
+
+        liste[0] = 1;
+        liste[1] = 2;
+        liste[2] = 3;
+        liste[3] = 4;
+
+        //System.out.println(liste[3]);
+        for(int i=0; i<liste.length; i++) {
+            System.out.println(liste[i]);
+        }
+    }
+}
---
+++ src/tablo/length.java
@@ -0,0 +1,2 @@
+package tablo;public class length {
+}
